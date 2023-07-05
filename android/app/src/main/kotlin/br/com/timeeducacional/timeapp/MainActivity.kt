f632bca82fd2369641495d44b1da4675484c54db
Magson Martins Magalhaes <magson@gmail.com>
2023-07-04 20:32:44 -0400

    Primeiro Commit

---
+++ android/app/src/main/kotlin/br/com/timeeducacional/timeapp/MainActivity.kt
@@ -0,0 +1,6 @@
+package br.com.timeeducacional.timeapp
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,54 @@
+import 'dart:io';                            // Add this import.
+import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+
+void main() {
+  runApp(
+    const MaterialApp(
+      home: WebViewApp(),
+    ),
+  );
+}
+
+class WebViewApp extends StatefulWidget {
+  const WebViewApp({Key? key}) : super(key: key);
+
+  @override
+  State<WebViewApp> createState() => _WebViewAppState();
+}
+
+class _WebViewAppState extends State<WebViewApp> {
+  @override
+  final controller = WebViewController()
+    ..setJavaScriptMode(JavaScriptMode.unrestricted)
+    ..setBackgroundColor(const Color(0x00000000))
+    ..setNavigationDelegate(
+      NavigationDelegate(
+        onProgress: (int progress) {
+          // Update loading bar.
+        },
+        onPageStarted: (String url) {},
+        onPageFinished: (String url) {},
+        onWebResourceError: (WebResourceError error) {},
+        onNavigationRequest: (NavigationRequest request) {
+          if (request.url.startsWith('https://www.youtube.com/')) {
+            return NavigationDecision.prevent;
+          }
+          return NavigationDecision.navigate;
+        },
+      ),
+    )
+
+    ..loadRequest(Uri.parse('https://ip.timeeducacional.com.br/dashboard'));
+
+
+
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Plantao App'),
+      ),
+      body: WebViewWidget(controller: controller),
+    );
+  }
+}
